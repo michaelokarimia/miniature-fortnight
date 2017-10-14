cd926cd0ad8b0bd028e77292d322f0a774c5a486
Michael Okarimia <git@michaelokarimia.com>
2017-10-14 16:31:42 +0100

    Stack implementation and test

---
+++ data-structure-tests/StackTests.cs
@@ -0,0 +1,42 @@
+﻿using data_structures;
+using NUnit.Framework;
+
+namespace data_structure_tests
+{
+    [TestFixture]
+    public class StackTests
+    {
+        [Test]
+        public void CanPushAndPeekItemOntoStack()
+        {
+            var stack = new Stack();
+
+            stack.Push(1);
+
+            Assert.That(stack.Peek(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void PopReturnsItem()
+        {
+            var stack = new Stack();
+
+            stack.Push(15);
+
+            Assert.That(stack.Pop(), Is.EqualTo(15));
+        }
+
+        [Test]
+        public void PopReturnsLastItem()
+        {
+            var stack = new Stack();
+
+            stack.Push(15);
+            stack.Push(100);
+
+            Assert.That(stack.Pop(), Is.EqualTo(100));
+            Assert.That(stack.Pop(), Is.EqualTo(15));
+
+        }
+    }
+}
---
+++ data-structures/Stack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace data_structures
+{
+    public class Stack
+    {
+        private readonly LinkedList list;
+
+        private int count;
+
+        public Stack()
+        {
+            list = new LinkedList();
+            count = 0;
+        }
+
+        public void Push(int value)
+        {
+            list.Append(value);
+            count++;
+        }
+
+        public int Peek()
+        {
+            return list.Traverse(null).Value;
+        }
+
+        public int Pop()
+        {
+            var last = list.Traverse(null);
+
+            var lastValue = last.Value;
+
+            list.Remove(last);
+
+            count--;
+
+            return lastValue;
+        }
+    }
+}
