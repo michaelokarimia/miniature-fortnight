603c910e18643feb62f3459f72cd145727526223
Michael Okarimia <git@michaelokarimia.com>
2017-10-14 15:37:15 +0100

    Implements DoubleyLinkedList

---
+++ data-structure-tests/DoubleyLinkedListTests.cs
@@ -0,0 +1,80 @@
+﻿using NUnit.Framework;
+using data_structures;
+
+namespace data_structure_tests
+{
+    [TestFixture]
+    public class DoubleyLinkedListsTests
+    {
+        [Test]
+        public void TraverseReturnsNullIfListIsEmpty()
+        {
+            var subject = new DoubleyLinkedList();
+
+            var result = subject.Traverse(null);
+
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void AppendsNodeToList()
+        {
+            var subject = new DoubleyLinkedList();
+
+            subject.Append(10);
+
+            Assert.That(subject.ToString(), Is.EqualTo("Index: 0, Value: 10\r\n")); 
+        }
+
+
+        [Test]
+        public void CanRemoveAHeadNode()
+        {
+            var subject = new DoubleyLinkedList();
+
+            var one = subject.Append(1);
+            subject.Append(2);
+
+            Assert.That(subject.ToString(), Is.EqualTo("Index: 0, Value: 1\r\nIndex: 1, Value: 2\r\n"));
+
+            subject.Remove(one);
+
+            Assert.That(subject.ToString(), Is.EqualTo("Index: 0, Value: 2\r\n"));
+
+        }
+
+        [Test]
+        public void CanRemoveMiddleNode()
+        {
+            var subject = new DoubleyLinkedList();
+
+            subject.Append(1);
+            var two = subject.Append(2);
+            subject.Append(3);
+
+            Assert.That(subject.ToString(), Is.EqualTo("Index: 0, Value: 1\r\nIndex: 1, Value: 2\r\nIndex: 2, Value: 3\r\n"));
+
+            subject.Remove(two);
+
+            Assert.That(subject.ToString(), Is.EqualTo("Index: 0, Value: 1\r\nIndex: 1, Value: 3\r\n"));
+
+        }
+
+        [Test]
+        public void InsertsAtMiddle()
+        {
+            var subject = new DoubleyLinkedList();
+
+            subject.Append(1);
+            var two = subject.Append(2);
+            subject.Append(3);
+
+            subject.InsertAt(two, 10);
+
+            Assert.That(subject.ToString(), Is.EqualTo("Index: 0, Value: 1\r\nIndex: 1, Value: 2\r\nIndex: 2, Value: 10\r\nIndex: 3, Value: 3\r\n"));
+
+        }
+
+
+    }
+}
---
+++ data-structures/DoubleyLinkedList.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Text;
+
+namespace data_structures
+{
+
+    public class Node
+    {
+        public Node(int val, Node next, Node previous)
+        {
+            this.Value = val;
+            this.Next = next;
+            this.Previous = previous;
+        }
+
+        
+
+        public int Value { get; }
+        public Node Previous { get; set; }
+        public Node Next { get; set; }
+    }
+
+    public class DoubleyLinkedList
+    {
+        private Node Head;
+
+        public Node Traverse(Func<Node, Node,bool> expression)
+        {
+            var current = this.Head;
+            Node last = null;
+
+            while (current != null)
+            {
+                if(expression != null && expression(current, last))
+                { break; }
+                last = current;
+                current = current.Next;
+            }
+
+            return last;
+        }
+
+        public Node Append(int value)
+        {
+            var insertNode = new Node(value, null, null);      
+            
+            //if list is empty insert new node at head
+            if(this.Head == null)
+            {
+                this.Head = insertNode;
+            }
+            else
+            {
+                //traverse list to get to the end, and insert new node there
+                var lastNode = this.Traverse(null);
+                insertNode.Previous = lastNode;
+                lastNode.Next = insertNode;
+            }
+
+            return insertNode;
+        }
+
+          public void Remove(Node deleteNode)
+        {
+            //check if head value is the node to remove
+            if (this.Head.Value.Equals(deleteNode.Value))
+            {
+                this.Head = Head.Next;
+            }
+            else
+            {
+                //set previous node's Next reference to point to the next node instead of the current node 
+
+                ///1 -><- 2 -><- 3 -><- 4 ->
+
+
+                var previous = deleteNode.Previous;
+                var next = deleteNode.Next;
+
+                if (next != null)
+                {
+                    next.Previous = previous;
+                }
+
+                previous.Next = next;
+            }
+        }
+
+        public Node InsertAt(Node node, int value)
+        {
+            // 1 -><- 2 -><- 8 -><-3
+            var newNode = new Node(value, node.Next, node);
+
+            node.Next.Previous = newNode;
+
+            node.Next = newNode;
+
+            return newNode;
+
+        }
+
+        public override string ToString()
+        {
+            var current = this.Head;
+
+            int counter = 0;
+            StringBuilder sb = new StringBuilder();
+
+            while (current != null)
+            {
+                sb.AppendLine(string.Format("Index: {0}, Value: {1}", counter, current.Value));
+                current = current.Next;
+                counter++;
+            }
+            return sb.ToString();
+
+        }
+
+    }
+}
+
+
