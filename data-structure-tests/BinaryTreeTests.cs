56cc3f136152f845174f93bb73a4620833c62a00
Michael Okarimia <git@michaelokarimia.com>
2017-10-17 14:07:20 +0100

    BinaryTree implementation with tests

---
+++ data-structure-tests/BinaryTreeTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using data_structures;
+
+namespace data_structure_tests
+{
+    [TestFixture]
+    public class BinaryTreeTests
+    {
+        [Test]
+        public void ReturnsItsValue()
+        {
+            var subject = new BinaryTree(9);
+
+            Assert.That(subject.Value, Is.EqualTo(9));
+        }
+
+
+        [Test]
+        public void AddsToLeftNode()
+        {
+            BinaryTree btree = new BinaryTree(1);
+
+            btree.AddLeft(2);
+
+            Assert.That(btree.LeftTree.Value, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void AddsToRightNode()
+        {
+            BinaryTree btree = new BinaryTree(1);
+
+            btree.AddRight(3);
+            Assert.That(btree.RightTree.Value, Is.EqualTo(3));
+
+        }
+    }
+}
---
+++ data-structures/BinaryTree.cs
@@ -0,0 +1,26 @@
+﻿namespace data_structures
+{
+    public class BinaryTree
+    {
+        private int _value;
+        
+        public BinaryTree(int val)
+            {
+                Value = val;
+            }
+
+            public BinaryTree LeftTree { get; private set; }
+            public BinaryTree RightTree { get; private set; }
+            public int Value { get => _value; private set => _value = value; }
+
+            public void AddLeft(int value)
+            {
+                LeftTree = new BinaryTree(value);
+            }
+
+            public void AddRight(int value)
+            {
+                RightTree = new BinaryTree(value);
+            }
+        }
+    }
